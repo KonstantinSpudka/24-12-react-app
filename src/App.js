c3b41b0d2473f144dec848b8e7052b877aee3aef
Alexandra <aspudka@gmail.com>
2023-01-02 20:15:26 +0200

    add ViewPort

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { Component } from 'react';
 
-import Counter from './Counter';
+// import StopWatch from './Components/StopWatch';
 
-function App() {
-  return <Counter />;
+// function App() {
+//   return <StopWatch />;
+// }
+
+// export default App;
+
+import StopWatch from './Components/ViewPortParametrs';
+
+class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    isVisible: false,
+  }
+}
+
+handleClick = () => {
+  const {isVisible} = this.state;
+  this.setState({isVisible: !isVisible})
+}
+
+  render() {
+    const {isVisible} = this.state;
+ return (
+  <>
+   <button onClick={this.handleClick}>
+    {isVisible ? 'Unmount' : 'Mount'}
+  </button>
+  {isVisible&&<StopWatch />}
+ 
+  </>
+ )
+ 
+
+    
+  }
 }
 
 export default App;
