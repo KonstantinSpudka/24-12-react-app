9db78b6a5d19b35ca4b8d86bf277f593add9c693
Alexandra <aspudka@gmail.com>
2023-01-05 16:15:46 +0200

    comit

--- src/App2.js
+++ src/App2.js
@@ -1,43 +1,43 @@
-import { Component } from 'react';
+  import { Component } from 'react';
 
-// import StopWatch from './Components/StopWatch';
+  // import StopWatch from './Components/StopWatch';
 
-// function App() {
-//   return <StopWatch />;
-// }
+  // function App() {
+  //   return <StopWatch />;
+  // }
 
-// export default App;
+  // export default App;
 
-import StopWatch from './Components/ViewPortParametrs/index2';
+  import StopWatch from './Components/ViewPortParametrs/index2';
 
-class App extends Component {
-constructor(props){
-  super(props);
-  this.state = {
-    isVisible: false,
+  class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isVisible: false,
+    }
   }
-}
 
-handleClick = () => {
-  const {isVisible} = this.state;
-  this.setState({isVisible: !isVisible})
-}
-
-  render() {
+  handleClick = () => {
     const {isVisible} = this.state;
- return (
-  <>
-   <button onClick={this.handleClick}>
-    {isVisible ? 'Unmount' : 'Mount'}
-  </button>
-  {isVisible&&<StopWatch />}
- 
-  </>
- )
- 
-
-    
+    this.setState({isVisible: !isVisible})
   }
-}
 
-export default App;
+    render() {
+      const {isVisible} = this.state;
+  return (
+    <>
+    <button onClick={this.handleClick}>
+      {isVisible ? 'Unmount' : 'Mount'}
+    </button>
+    {isVisible&&<StopWatch />}
+  
+    </>
+  )
+  
+
+      
+    }
+  }
+
+  export default App;
